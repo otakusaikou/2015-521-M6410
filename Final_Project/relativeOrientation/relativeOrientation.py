fad4d80db8ab4e4990dd71f8b9f6310a609bd2b5
otakusaikou <otakuzyoutou@gmail.com>
2016-11-27 21:40:25 +0900

    [Update] Improved the efficiency of iteration process

--- Final_Project/relativeOrientation/relativeOrientation.py
+++ Final_Project/relativeOrientation/relativeOrientation.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from sympy import symbols, sin, cos, Matrix, lambdify, matrix2numpy
+from sympy import symbols, sin, cos, Matrix, lambdify
 from math import degrees as deg
 import numpy as np
 import sys
 
 
-# LANG = sys.stdout.encoding          # Get system language code
-LANG = "utf-8"
 np.set_printoptions(suppress=True)  # Disable scientific notation for numpy
 
 
@@ -51,173 +49,178 @@ def getEqns(x0, y0, f, XL, YL, ZL, omega, phi, kappa, x, y, XA, YA, ZA):
     return Matrix([Fx, Fy])
 
 
-def main():
-    # Define image coordinate of conjugate points
-    Lx = np.array([-4.87, 89.296, 0.256, 90.328, -4.673, 88.591])
-    Ly = np.array([1.992, 2.706, 84.138, 83.854, -86.815, -85.269])
-    Rx = np.array([-97.920, -1.485, -90.906, -1.568, -100.064, -0.973])
-    Ry = np.array([-2.91, -1.836, 78.980, 79.482, -95.733, -94.312])
+def relativeOrientation(Lx, Ly, Rx, Ry, f, x0, y0):
+    XL = YL = OL = PL = KL = 0          # E.O. parameters for left photo
+    ZL = f                              # O=Omega, P=Phi, K=Kappa
 
-    # Define interior orientation parameters
-    f = 152.113
+    XR = abs(Lx - Rx).mean()    # Part of E.O. parameters for right photo
 
     # Define initial values
-    XL0 = YL0 = OmegaL0 = PhiL0 = KappaL0 = 0
-    ZL0 = ZR0 = f
-    XR0 = abs(Lx - Rx).mean()
-    YR0 = OmegaR0 = PhiR0 = KappaR0 = 0
+    ZR0 = f
+    YR0 = OR0 = PR0 = KR0 = 0
 
     # Define initial coordinate for object points
-    # X0 = np.array(Lx)
-    # Y0 = np.array(Ly)
-    # Z0 = np.zeros(len(Lx))
-    X0 = XR0 * Lx / (Lx - Rx)
-    Y0 = XR0 * Ly / (Lx - Rx)
-    Z0 = f - (XR0 * f / (Lx - Rx))
+    X0 = XR * Lx / (Lx - Rx)
+    Y0 = XR * Ly / (Lx - Rx)
+    Z0 = f - (XR * f / (Lx - Rx))
 
     # Define symbols
-    fs, x0s, y0s = symbols("f x0 y0")
-    XLs, YLs, ZLs, OmegaLs, PhiLs, KappaLs = symbols(
-        u"XL YL ZL ωL, φL, κL".encode(LANG))
-
-    XRs, YRs, ZRs, OmegaRs, PhiRs, KappaRs = symbols(
-        u"XR YR ZR ωR, φR, κR".encode(LANG))
-
+    YRs, ZRs, ORs, PRs, KRs = symbols("YR ZR OR PR KR")
     xls, yls, xrs, yrs = symbols("xl yl xr yr")
     XAs, YAs, ZAs = symbols("XA YA ZA")
 
+    # Define a symbol array for unknown parameters
+    ls = np.array([ORs, PRs, KRs, YRs, ZRs, XAs, YAs, ZAs])
+
     # List observation equations
     F1 = getEqns(
-        x0s, y0s, fs, XLs, YLs, ZLs, OmegaLs, PhiLs, KappaLs,
-        xls, yls, XAs, YAs, ZAs)
-
+        x0, y0, f, XL, YL, ZL, OL, PL, KL, xls, yls, XAs, YAs, ZAs)
     F2 = getEqns(
-        x0s, y0s, fs, XRs, YRs, ZRs, OmegaRs, PhiRs, KappaRs,
-        xrs, yrs, XAs, YAs, ZAs)
+        x0, y0, f, XR, YRs, ZRs, ORs, PRs, KRs, xrs, yrs, XAs, YAs, ZAs)
+    F = Matrix([F1, F2])
 
-    # Create lists for substitution of initial values and constants
-    var1 = np.array([x0s, y0s, fs, XLs, YLs, ZLs, OmegaLs, PhiLs, KappaLs,
-                     xls, yls, XAs, YAs, ZAs])
-    var2 = np.array([x0s, y0s, fs, XRs, YRs, ZRs, OmegaRs, PhiRs, KappaRs,
-                     xrs, yrs, XAs, YAs, ZAs])
+    # Create function objects for matrices BTB, BTf and F
+    JFx = F.jacobian(ls)
+    FuncBTB = lambdify(ls, (JFx.T*JFx), 'numpy')
+    FuncBTf = lambdify(
+                       np.append(ls, [xls, yls, xrs, yrs]),
+                       (JFx.T*-F), 'numpy')
 
-    # Define a symbol array for unknown parameters
-    l = np.array([OmegaRs, PhiRs, KappaRs, YRs, ZRs, XAs, YAs, ZAs])
+    FuncF0 = lambdify(np.append(ls, [xls, yls, xrs, yrs]), F, 'numpy')
 
-    # Compute coefficient matrix
-    JF1 = F1.jacobian(l)
-    JF2 = F2.jacobian(l)
+    # Array for the observables and initial values
+    XA = X0
+    YA = Y0
+    ZA = Z0
+    OR = np.zeros(Lx.shape) + OR0
+    PR = np.zeros(Lx.shape) + PR0
+    KR = np.zeros(Lx.shape) + KR0
+    YR = np.zeros(Lx.shape) + YR0
+    ZR = np.zeros(Lx.shape) + ZR0
+    l = np.dstack(
+        ([OR, PR, KR, YR, ZR, XA, YA, ZA, Lx, Ly, Rx, Ry])).reshape(-1, 12)
 
-    # Create function objects for two parts of coefficient matrix and f matrix
-    B1 = lambdify(tuple(var1), JF1, modules='sympy')
-    B2 = lambdify(tuple(var2), JF2, modules='sympy')
-    F01 = lambdify(tuple(var1), -F1, modules='sympy')
-    F02 = lambdify(tuple(var2), -F2, modules='sympy')
+    numPts = len(Lx)
 
-    X = np.ones(1)      # Initial value for iteration
-    lc = 1              # Loop counter
-    while abs(X.sum()) > 10**-12:
-        B = np.matrix(np.zeros((4 * len(Lx), 5 + 3 * len(Lx))))
-        F0 = np.matrix(np.zeros((4 * len(Lx), 1)))
-        # Column index which is used to update values of B and f matrix
-        j = 0
-        for i in range(len(Lx)):
-            # Create lists of initial values and constants
-            val1 = np.array([0, 0, f, XL0, YL0, ZL0, OmegaL0, PhiL0, KappaL0,
-                             Lx[i], Ly[i], X0[i], Y0[i], Z0[i]])
-            val2 = np.array([0, 0, f, XR0, YR0, ZR0, OmegaR0, PhiR0, KappaR0,
-                             Rx[i], Ry[i], X0[i], Y0[i], Z0[i]])
-            # For coefficient matrix B
-            b1 = matrix2numpy(B1(*val1)).astype(np.double)
-            b2 = matrix2numpy(B2(*val2)).astype(np.double)
-            B[i*4:i*4+2, :5] = b1[:, :5]
-            B[i*4:i*4+2, 5+j*3:5+(j+1)*3] = b1[:, 5:]
-            B[i*4+2:i*4+4, :5] = b2[:, :5]
-            B[i*4+2:i*4+4, 5+j*3:5+(j+1)*3] = b2[:, 5:]
+    # Start iteration process
+    lc = 1                      # Loop count
+    C = 2.0                     # Stopping criteria
+    VTV0 = 1.0                  # Initial value of residual
+    while C > 10**-12:
+        BTB = FuncBTB(*np.hsplit(l, 12)[:-4])
+        BTf = FuncBTf(*np.hsplit(l, 12))
 
-            # For constant matrix f
-            f01 = matrix2numpy(F01(*val1)).astype(np.double)
-            f02 = matrix2numpy(F02(*val2)).astype(np.double)
-            F0[i*4:i*4+2, :5] = f01
-            F0[i*4+2:i*4+4, :5] = f02
-            j += 1
+        N = np.matrix(np.zeros((5+3*numPts, 5+3*numPts)))
+        t = np.matrix(np.zeros((5+3*numPts, 1)))
+
+        N[:5, :5] = BTB[:5, :5].sum(axis=2).reshape(5, 5)
+        t[:5, :] = BTf[:5, :].sum(axis=2).reshape(5, 1)         # Upper left
+        for i in range(numPts):
+            N[5+3*i:5+3*(i+1), :5] = \
+                BTB[5:, :5, i].reshape(3, 5)
+            t[5+3*i:5+3*(i+1), :] = \
+                BTf[5:, :, i].reshape(3, 1)                     # Lower left
+
+            for j in range(numPts):
+                N[:5, 5+3*j:5+3*(j+1)] = \
+                    BTB[:5, 5:, j].reshape(5, 3)                # Upper right
+                if i == j:
+                    N[5+3*i:5+3*(i+1), 5+3*j:5+3*(j+1)] = \
+                        BTB[5:, 5:, i].reshape(3, 3)            # Lower right
 
         # Solve unknown parameters
-        N = B.T * B             # Compute normal matrix
-        t = B.T * F0            # Compute t matrix
-        X = N.I * t             # Compute the unknown parameters
+        X = N.I * t
+
+        # Compute residual
+        F0 = np.matrix(FuncF0(*np.hsplit(l, 12))[:, 0, :, 0].T.reshape(-1, 1))
+        VTV1 = X.T*N*X - t.T*X - X.T*t + F0.T*F0
+
+        # Update the stopping criteria
+        C = abs(VTV1/VTV0 - 1)
+        VTV0 = VTV1
 
         # Update initial values
-        OmegaR0 += X[0, 0]
-        PhiR0 += X[1, 0]
-        KappaR0 += X[2, 0]
-        YR0 += X[3, 0]
-        ZR0 += X[4, 0]
-        X0 += np.array(X[5::3, 0]).flatten()
-        Y0 += np.array(X[6::3, 0]).flatten()
-        Z0 += np.array(X[7::3, 0]).flatten()
+        l[:, 0] += X[0, 0]
+        l[:, 1] += X[1, 0]
+        l[:, 2] += X[2, 0]
+        l[:, 3] += X[3, 0]
+        l[:, 4] += X[4, 0]
+        l[:, 5] += np.array(X[5::3, 0]).ravel()
+        l[:, 6] += np.array(X[6::3, 0]).ravel()
+        l[:, 7] += np.array(X[7::3, 0]).ravel()
 
         # Output messages for iteration process
-        print "Iteration count: %d" % lc, u"|ΔX| = %.6f".encode(LANG) \
-            % abs(X.sum())
+        print "Iteration count: %d, " % lc, "VTV = %.6f" % VTV1
+
         lc += 1         # Update Loop counter
 
-    # Compute residual vector
-    V = F0 - B * X
-
-    # Compute error of unit weight
-    s0 = ((V.T * V)[0, 0] / (B.shape[0] - B.shape[1]))**0.5
+    # Compute sigma0
+    s0 = (VTV1[0, 0] / (4*numPts - (5+3*(numPts))))**0.5
 
     # Compute other informations
-    # Sigmall = np.eye(B.shape[0])
     SigmaXX = s0**2 * N.I
-    # SigmaVV = s0**2 * (Sigmall - B * N.I * B.T)
-    # Sigmallhat = s0**2 * (Sigmall - SigmaVV)
-    param_std = np.sqrt(np.diag(SigmaXX))
-    pho_res = np.array(V).flatten()
-    # pho_res = np.sqrt(np.diag(SigmaVV))
+    paramStd = np.sqrt(np.diag(SigmaXX))
 
     # Output results
     print "\nExterior orientation parameters:"
     print ("%9s"+" %9s"*3) % ("Parameter", "Left pho", "Right pho", "SD right")
     print "%-10s %8.4f %9.4f %9.4f" % (
-        "Omega(deg)", deg(OmegaL0), deg(OmegaR0), deg(param_std[0]))
+        "Omega(deg)", deg(OL), deg(l[0, 0]), deg(paramStd[0]))
     print "%-10s %8.4f %9.4f %9.4f" % (
-        "Phi(deg)", deg(PhiL0), deg(PhiR0), deg(param_std[1]))
+        "Phi(deg)", deg(PL), deg(l[0, 1]), deg(paramStd[1]))
     print "%-10s %8.4f %9.4f %9.4f" % (
-        "Kappa(deg)", deg(KappaL0), deg(KappaR0), deg(param_std[2]))
+        "Kappa(deg)", deg(KL), deg(l[0, 2]), deg(paramStd[2]))
     print "%-10s %8.4f %9.4f" % (
-        "XL", XL0, XR0)
+        "XL", XL, XR)
     print "%-10s %8.4f %9.4f %9.4f" % (
-        "YL", YL0, YR0, param_std[3])
+        "YL", YL, l[0, 3], paramStd[3])
     print "%-10s %8.4f %9.4f %9.4f\n" % (
-        "ZL", ZL0, ZR0, param_std[4])
+        "ZL", ZL, l[0, 4], paramStd[4])
 
-    print "Object space coordinates:"
+    print "Object point coordinates:"
     print ("%5s"+" %9s"*6) % ("Point", "X", "Y", "Z", "SD-X", "SD-Y", "SD-Z")
 
     for i in range(len(X0)):
-        print ("%5s"+" %9.4f"*6) % (
-            chr(97+i), X0[i], Y0[i], Z0[i],
-            param_std[3*i+5],
-            param_std[3*i+6],
-            param_std[3*i+7])
+        print ("P%d"+" %9.4f"*6) % (
+            i, l[i, 5], l[i, 6], l[i, 7],
+            paramStd[3*i+5],
+            paramStd[3*i+6],
+            paramStd[3*i+7])
 
-    print "\nPhoto coordinate residuals:"
-    print ("%5s"+" %9s"*4) % ("Point", "xl-res", "yl-res", "xr-res", "yr-res")
-    for i in range(len(X0)):
-        print ("%5s"+" %9.4f"*4) % (
-            chr(97+i), pho_res[2*i], pho_res[2*i+1],
-            pho_res[2*i+len(X0)*2], pho_res[2*i+len(X0)*2+1])
-    print ("\n%5s"+" %9.4f"*4+"\n") % (
-        "RMS",
-        np.sqrt((pho_res[0:len(X0)*2:2]**2).mean()),
-        np.sqrt((pho_res[1:len(X0)*2+1:2]**2).mean()),
-        np.sqrt((pho_res[len(X0)*2::2]**2).mean()),
-        np.sqrt((pho_res[len(X0)*2+1::2]**2).mean()))
+    print "\nSigma0 : %.4f" % s0
+    print "Degree of freedom: %d" % (4*numPts - (5+3*(numPts)))
 
-    print "Standard error of unit weight : %.4f" % s0
-    print "Degree of freedom: %d" % (B.shape[0] - B.shape[1])
+    return l
+
+
+def main():
+    # Load I.O. parameters and image coordinate of conjugate points
+    if len(sys.argv) == 1:      # Use test data if nothing given
+        outputFileName = "result.txt"
+        Lx = np.array([-4.87, 89.296, 0.256, 90.328, -4.673, 88.591])
+        Ly = np.array([1.992, 2.706, 84.138, 83.854, -86.815, -85.269])
+        Rx = np.array([-97.920, -1.485, -90.906, -1.568, -100.064, -0.973])
+        Ry = np.array([-2.91, -1.836, 78.980, 79.482, -95.733, -94.312])
+        f = 152.113     # Focal length in mm
+
+    else:
+        inputFileName, IOFileName, outputFileName = tuple(sys.argv[1:])
+        imgPts = np.genfromtxt(inputFileName, skip_header=1)
+        Lx, Ly, Rx, Ry = np.hsplit(imgPts, 4)
+        f = np.genfromtxt(IOFileName)[0]
+
+    x0 = y0 = 0
+
+    # Compute the relative orientation parameters and object point coordinates
+    RO = relativeOrientation(Lx, Ly, Rx, Ry, f, x0, y0)
+
+    # Write out the results
+    np.savetxt(
+        outputFileName,
+        RO[:, 5:8],
+        fmt="%.8f %.8f %.8f",
+        # header="X Y Z R G B",
+        comments='')
 
     return 0
 
